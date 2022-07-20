ac460cfe6091744c9fb02cea5b4f5d81d8ed352c
vaniamaksimov <vaniamaksimov@gmail.com>
2022-07-20 21:13:19 +0300

    change urls.py UserMeViewSet

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CategoriesViewSet, CommentsViewSet, GenreViewSet, ReviewsViewSet,
-                    TitlesViewSet, UserViewSet)
+                    TitlesViewSet, UserMeViewSet, UserViewSet)
 
 api_router_v1 = DefaultRouter()
 api_router_v1.register('categories', CategoriesViewSet, basename='categories')
@@ -12,7 +12,7 @@ api_router_v1.register('titles', TitlesViewSet, basename='titles')
 api_router_v1.register(r'titles/(?P<title_id>\d+)/reviews', ReviewsViewSet, basename='reviews')
 api_router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments', CommentsViewSet, basename='comments')
 api_router_v1.register('users', UserViewSet, basename='users')
-api_router_v1.register('users/me', UserViewSet, basename='user')
+api_router_v1.register('users/me', UserMeViewSet, basename='user')
 
 urlpatterns = [
     path('v1/', include(api_router_v1.urls)),
